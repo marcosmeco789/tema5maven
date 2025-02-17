21cdfb56297d8cda5a63b1361346f6c7c4f9c951
marcosmeco789 <marcosmeco987@gmail.com>
2025-02-17 16:58:28 +0100

    17/02

--- src/main/java/ejem1/Deportistas.java
+++ src/main/java/ejem1/Deportistas.java
@@ -16,7 +16,7 @@ public class Deportistas {
 
     public void abrirConexion(String bd, String servidor, String usuario,
             String password) {
-        try { 
+        try {
             String url = String.format("jdbc:mariadb://localhost:3306/ad_tema6?useServerPrepStmts=true", "usuario",
                     "contraseña");
             // Establecemos la conexión con la BD
@@ -32,14 +32,14 @@ public class Deportistas {
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("Código error: " + e.getErrorCode());
         } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
+            e.printStackTrace();
+        }
     }
 
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_XML)
-    public Response ej1() throws SQLException{
+    public Response ej2() throws SQLException {
         abrirConexion("ad_tema6", "localhost", "root", "");
         ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas;");
         ArrayList<Deportista> deportistas = new ArrayList<>();
@@ -53,15 +53,16 @@ public class Deportistas {
             deportistas.add(d);
         }
         conexion.close();
-        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {};
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
 
         return Response.ok(deportistaEntity).build();
     }
 
     @GET
     @Path("/{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response ej2(@PathParam("id") int id) throws SQLException{
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response ej3(@PathParam("id") int id) throws SQLException {
         abrirConexion("ad_tema6", "localhost", "root", "");
         ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where id = " + id + ";");
         Deportista d = new Deportista();
@@ -79,22 +80,190 @@ public class Deportistas {
     @GET
     @Path("/deporte/{nombreDeporte}")
     @Produces(MediaType.APPLICATION_XML)
-    public Response ej3() throws SQLException{
+    public Response ej4(@PathParam("nombreDeporte") String nombreDeporte) throws SQLException {
 
-        
-        
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ResultSet rs = conexion.createStatement()
+                .executeQuery("select * from deportistas where deporte = '" + nombreDeporte + "';");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+        conexion.close();
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
+
+        return Response.ok(deportistaEntity).build();
     }
 
-    
+    @GET
+    @Path("/activos")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ej5() throws SQLException {
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where activo = 1;");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+        conexion.close();
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
+
+        return Response.ok(deportistaEntity).build();
+    }
+
+    @GET
+    @Path("/retirados")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ej6() throws SQLException {
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where activo = 0;");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+        conexion.close();
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
+
+        return Response.ok(deportistaEntity).build();
+    }
+
+    @GET
+    @Path("/masculinos")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ej7() throws SQLException {
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where genero = 'Masculino';");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+        conexion.close();
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
+
+        return Response.ok(deportistaEntity).build();
+    }
+
+    @GET
+    @Path("/femeninos")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ej8() throws SQLException {
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where genero = 'Femenino';");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+        conexion.close();
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas) {
+        };
+
+        return Response.ok(deportistaEntity).build();
+    }
+
+    @GET
+    @Path("/xg")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ej9() throws SQLException {
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+
+        ResultSet rs = conexion.createStatement().executeQuery("select * from deportistas where genero = 'Masculino'");
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+
+        rs = conexion.createStatement().executeQuery("select * from deportistas where genero = 'Femenino'");
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+
+        }
+        conexion.close();
+
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas){};
+        return Response.ok(deportistaEntity).build();
+    }
+
+
+    // ej10: Activos por deporte (/deporte/{nombreDeporte}/activos): Lista los deportistas activos de un deporte.
+    @GET
+    @Path("/deporte/{nombreDeporte}/activos")
+    @Produces (MediaType.APPLICATION_XML)
+    public Response ej10(@PathParam("nombreDeporte") String nombreDeporte) throws SQLException{
+        abrirConexion("ad_tema6", "localhost", "root", "");
+        String query = "SELECT * from deportistas WHERE deporte = '"+nombreDeporte+"' AND activo = 1";
+        ResultSet rs = this.conexion.createStatement().executeQuery(query);
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+
+        while (rs.next()) {
+            Deportista d = new Deportista();
+            d.setId(rs.getInt(1));
+            d.setNombre(rs.getString(2));
+            d.setActivo(rs.getBoolean(3));
+            d.setGenero(rs.getString(4));
+            d.setDeporte(rs.getString(5));
+            deportistas.add(d);
+        }
+
+        GenericEntity<ArrayList<Deportista>> deportistaEntity = new GenericEntity<ArrayList<Deportista>>(deportistas){};
+        return Response.ok(deportistaEntity).build();
+    }
+
+
 
     public static void main(String[] args) {
         Deportistas dp = new Deportistas();
 
         dp.abrirConexion("ad_tema6", "localhost", "root", "");
         try {
-            
+            dp.ej3(0);
+
         } catch (SQLException e) {
-            
             e.printStackTrace();
         }
     }
